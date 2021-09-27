2bb079bafebed8185ebefc541f6c81128593ff28
Emilian Roman <roman.emilian@outlook.com>
2021-09-28 03:21:41 +0800

    Improve program source code, usage, documentation, etc.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Mono.Options;
@@ -9,74 +8,78 @@ using static System.DateTime;
 using static System.Diagnostics.Process;
 using static System.Environment;
 using static System.Globalization.CultureInfo;
+using static System.Guid;
 using static System.IO.File;
 using static System.IO.Path;
 
 namespace Gunloader
 {
-  internal class Program
+  /**
+   * Gunloader Program.
+   */
+  public static class Program
   {
     public static readonly OptionSet OptionSet = new()
     {
       {
-        "url=", "url of the gundober playlist",
-        s => URL = s
-      },
-      {
-        "records=", "path to records file with times and titles",
+        "records=|timestamps=|cue=", "path to records file with times and titles",
         s => Records = new FileInfo(s)
       },
       {
-        "download=|file=", "path to a downloaded gundober playlist file",
-        s => Download = new FileInfo(s)
+        "compilation=|source=|file=", "path to a downloaded gundober compilation file",
+        s => Source = new FileInfo(s)
       },
       {
-        "album=", "album name to assign to the tracks",
+        "album=", "album name to move tracks to, and to assign to the tracks' metadata",
         s => Album = s
       }
     };
 
-    public static string   URL      { get; set; } = string.Empty;
-    public static FileInfo Records  { get; set; } = new(Combine(CurrentDirectory, "tracks.txt"));
-    public static FileInfo Download { get; set; } = new(Combine(CurrentDirectory, Guid.NewGuid().ToString()));
-    public static string   Album    { get; set; } = string.Empty;
 
-    private static void Main(string[] args)
+    public static FileInfo Records { get; set; } = new(Combine(CurrentDirectory, "tracks.txt"));
+    public static FileInfo Source  { get; set; } = new(Combine(CurrentDirectory, NewGuid().ToString()));
+    public static string   Album   { get; set; } = string.Empty; /* for the metadata and output directory name */
+
+    public static void Main(string[] args)
     {
       OptionSet.WriteOptionDescriptions(Out);
       OptionSet.Parse(args);
 
-      if (!string.IsNullOrWhiteSpace(URL) && !Download.Exists)
-        Start(new ProcessStartInfo
-        {
-          FileName = "youtube-dl",
-          Arguments = $"-k {URL} " +
-                      $"-o {Download.FullName}"
-        });
+      var compilation = Source.FullName;                /* source path   */
+      var records     = ReadAllLines(Records.FullName); /* track records */
 
-      var album    = Album;
-      var download = Download.FullName;
-      var records  = ReadAllLines(Records.FullName);
+      if (!string.IsNullOrWhiteSpace(Album))
+        Directory.CreateDirectory(Album);
+
+      /**
+       * TODO: Parallel implementation for contributing towards a more sustainable future for our planet.
+       * TODO: Make this a wee bit more object-oriented? For the purists and masochists who seek misery here...
+       */
 
       foreach (var record in records)
       {
         var split  = record.Split(' ');
-        var number = split[0];
-        var start  = split[1];
-        var title  = string.Join(' ', split.Skip(2));
-        var cover  = ParseExact(start, "H:mm:ss", InvariantCulture).AddSeconds(30);
+        var number = split[0];                        /* album track number  */
+        var start  = split[1];                        /* track starting time */
+        var title  = string.Join(' ', split.Skip(2)); /* track title         */
+        var cover = ParseExact(start, "H:mm:ss", InvariantCulture)
+          .AddSeconds(30); /* (start time + 30 seconds) has correct thumbnail */
+
+        /**
+         * Normalise output filename for Windows & Linux systems.
+         */
 
         var normalised = new[]
         {
-          "<",
-          ">",
-          ":",
-          "\"",
-          "/",
-          "\\",
-          "|",
-          "?",
-          "*"
+          "<",  /* win */
+          ">",  /* win */
+          ":",  /* win */
+          "\"", /* win */
+          "/",  /* lin */
+          "\\", /* win */
+          "|",  /* win */
+          "?",  /* win */
+          "*"   /* win */
         }.Aggregate(title, (current, character) =>
           current.Replace(character, ""));
 
@@ -84,24 +87,40 @@ namespace Gunloader
           ? string.Empty
           : records[IndexOf(records, record) + 1].Split(' ')[1];
 
-        WriteLine($"{start} -- {cover:H:mm:ss}");
+        WriteLine(new string('-', 80));
+        WriteLine($"{number}. {title}");
+        WriteLine($"{start} {end}");
+
+        /**
+         * Extract the MP3 from the source file for the current track.
+         */
 
         Start(new ProcessStartInfo
         {
           FileName = "ffmpeg",
-          Arguments = $"-i {download} "                                      +
+          Arguments = $"-i {compilation} "                                   +
                       $"-ss {start} "                                        +
                       $"{(!string.IsNullOrEmpty(end) ? $"-to {end}" : "")} " +
                       $"{number}.mp3"
         })?.WaitForExit();
 
+        /**
+         * Extract the cover art from the source file for the current track.
+         */
+
         Start(new ProcessStartInfo
         {
           FileName = "ffmpeg",
-          Arguments = $"-i {download} -ss {cover:H:mm:ss} " +
-                      "-vframes 1 "                         +
+          Arguments = $"-i {compilation} -ss {cover:H:mm:ss} " +
+                      "-vframes 1 "                            +
                       $"{number}.png"
         })?.WaitForExit();
+
+        /**
+         * Embed cover art & album title metadata within the track.
+         *
+         * TODO: Figure out a way of extracting the audio *and* adding the data in one pass.
+         */
 
         Start(new ProcessStartInfo
         {
@@ -115,14 +134,20 @@ namespace Gunloader
                       "-metadata:s:v title=\"Album cover\" "     +
                       "-metadata:s:v comment=\"Cover (front)\" " +
                       $"-metadata title=\"{title}\" "            +
-                      $"-metadata album=\"{album}\" "            +
+                      $"-metadata album=\"{Album}\" "            +
                       $"{number}~1.mp3"
         })?.WaitForExit();
 
-        Move($"{number}~1.mp3", $"{number}. {normalised}.mp3");
+        /**
+         * Move file to the normalised name of the track (within album directory if exists), and remove temporary files.
+         */
+
+        Move($"{number}~1.mp3", Combine(Album ?? string.Empty, $"{number}. {normalised}.mp3"));
 
         Delete($"{number}.png");
         Delete($"{number}.mp3");
+
+        WriteLine(new string('=', 80));
       }
     }
   }
