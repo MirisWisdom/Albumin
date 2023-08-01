d114e2f300ef4c15ce63e3ad9ec83944ab5cefda
Emilian Roman <git@miris.design>
2023-08-01 18:20:11 +0800

    Revise default GUI config values
    Signed-off-by: Emilian Roman <git@miris.design>

--- gui/config/app.php
+++ gui/config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'YouTube Albumin'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,7 +66,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://albumin.miris.design'),
 
     'asset_url' => env('ASSET_URL'),
 
